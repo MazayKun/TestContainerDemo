ee54303e202954a8e3c826c43e96393a67da9b74
MazayKun <kir99916@gmail.com>
2023-09-16 15:41:46 +0300

    Дозалил остатки

---
+++ src/test/resources/db.scripts/test-scripts.sql
@@ -0,0 +1,3 @@
+insert into manager(id, label) values (1, 'Test Manager 1');
+insert into client(id, order_column, name, manager_id) values (1, 1, 'Test Client 1', 1);
+insert into client_details(client_id, info) values (1, 'Client 1 details');
